9bf5449e9decf087a4585c14d5d5534a00fedc15
bennet287 <77139673+bennet287@users.noreply.github.com>
2024-12-17 18:13:41 +0100

    Added new Testcase for checking the Test result Trend

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void creatingDummyTestCaseForCheckingTheTestResultTrend() {
+    String dummyCase = "This is a Dummy Test case";
+    assertThat(dummyCase).isNotNull();
+  }
 }
